6d028d37290854705bd9fba34325b8921a9fb4c0
blelian <bullsurus@gmail.com>
2025-03-27 09:11:41 +0200

    changed the attribute _commentName to _commenterName as it better describes it.

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,15 +1,15 @@
 using System;
 public class Comment
 {
-    public string _commentName;
+    public string _commenterName;
     public string _commentText;
     public Comment(string name, string text)
     {
-        _commentName = name;
+        _commenterName = name;
         _commentText = text;
     }
     public void DisplayComment()
     {
-        Console.WriteLine($"{_commentName}: {_commentText}");
+        Console.WriteLine($"{_commenterName}: {_commentText}");
     }
 }
