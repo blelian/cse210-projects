a1db11ac150c372d22dbf7b0f21d8326b8d70dbb
blelian <bullsurus@gmail.com>
2025-04-02 15:43:15 +0200

    Added new classes to the Homework program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Stanley Kwembe", "Fractions", "Section 7.3 Problems 8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Blessing Mpofu", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
