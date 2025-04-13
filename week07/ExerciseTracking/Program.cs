dde7e75bf4950d41cbea33defd5190bd570dff95
blelian <bullsurus@gmail.com>
2025-04-13 17:03:02 +0200

    Created a new program and also included a screenshot.

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("13 April 2025", 30, 4.8),
+            new Cycling("13 April 2025", 45, 20.0),
+            new Swimming("13 April 2025", 30, 40)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
